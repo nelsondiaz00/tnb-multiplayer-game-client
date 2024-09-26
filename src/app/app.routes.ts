8859aa1d8bb4d98867d13d4499bc3be523959d26
gianmt-06 <moragian6@gmail.com>
2024-09-26 09:36:21 -0500

    sync

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,7 @@ import { MatchListComponent } from './match-management/match-list/match-list.com
 export const routes: Routes = [
   { path: 'match-list', component: MatchListComponent },
   { path: 'game-view', component: GameViewComponent },
-];
+  { path: 'admin/hero', component: GameViewComponent },
+  { path: 'admin/inventory', component: GameViewComponent },
+  ];
 export class AppRoutingModule {}
