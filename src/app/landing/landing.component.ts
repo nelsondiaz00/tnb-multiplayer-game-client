c0b50f6af9cc7d95d6ecdd0548278ed17751b6a8
nelsondiaz00 <nelsondiaz1404@gmail.com>
2024-09-25 19:12:04 -0500

    arreglo de landing y varios errores

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,12 +1,103 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChatBotViewComponent } from '../chatbot/chat-bot-view/chat-bot-view.component';
+import { MatchManagementViewComponent } from '../match-management/match-management-view/match-management-view.component';
+import { InfoUserComponent } from '../profile/info-user/info-user.component';
+import { SignInComponent } from '../profile/sign-in/sign-in.component';
+import { SignUpComponent } from '../profile/sign-up/sign-up.component';
+import { HeroInventoryComponent } from '../inventary/hero-inventory/hero-inventory.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [
+    ChatBotViewComponent,
+    MatchManagementViewComponent,
+    InfoUserComponent,
+    SignInComponent,
+    SignUpComponent,
+    HeroInventoryComponent,
+    CommonModule,
+  ],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
+  title = 'tnb-multiplayer-client';
+  showGameView = false;
+  showInfoUser = false;
+  showInventary = false;
+  showManagementMatch = false;
+  showHeader = false;
+  showSignIn = false;
+  showRegister = false;
+  showLanding = true;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  toggleGameView(show: boolean) {
+    this.showHeader = false;
+    this.showGameView = show;
+    this.cdr.detectChanges();
+  }
+
+  
+
+  showInfoUserComponent() {
+    this.hideViews();
+    this.showInfoUser = true;
+    this.cdr.detectChanges();
+  }
+
+  showInventaryComponent() {
+    this.hideViews();
+    this.showInventary = true;
+    this.cdr.detectChanges();
+  }
+
+  // showHeaderComponent() {
+  //   this.hideViews();
+  //   this.showHeader = true;
+  //   this.cdr.detectChanges();
+  // }
+
+  showManagementMatchComponent() {
+    this.hideViews();
+    this.showHeader = true;
+    this.showManagementMatch = true;
+    this.cdr.detectChanges();
+  }
+
+  showSignInComponent() {
+    this.hideViews();
+    this.showSignIn = true;
+    this.cdr.detectChanges();
+  }
+
+  showRegisterComponent() {
+    console.log('showRegisterComponent');
+    this.hideViews();
+    this.showRegister = true;
+    this.cdr.detectChanges();
+  }
+
+  showLandingComponent() {
+    this.hideViews();
+    this.showLanding = true;
+  }
+
+  showHeaderComponent() {
+    this.showHeader = true;
+    this.cdr.detectChanges();
+  }
+
+  hideViews() {
+    this.showGameView = false;
+    this.showInfoUser = false;
+    this.showInventary = false;
+    this.showManagementMatch = false;
+    this.showSignIn = false;
+    this.showRegister = false;
+    this.showLanding = false;
+    this.cdr.detectChanges();
+  }
 }
