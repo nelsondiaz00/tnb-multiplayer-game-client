ed86da93389c6d04be530829ddfd0abbcb02cd62
JD93Lopez <juandavid.jdlea@gmail.com>
2024-10-09 08:56:46 -0500

    Pop up

--- src/app/inventary/client-inventory/hero-inventory/hero-inventory.component.ts
+++ src/app/inventary/client-inventory/hero-inventory/hero-inventory.component.ts
@@ -16,7 +16,7 @@ export class HeroInventoryComponent {
   currentPage: number = 1;
   itemsPerPage: number = 12;
 
-  constructor(private inventoryService: ClientInventoryService) {}
+  constructor(private inventoryService: ClientInventoryService) { }
 
   setFilter(filter: string): void {
     this.filter = filter;
@@ -37,20 +37,36 @@ export class HeroInventoryComponent {
         : 0) +
       (this.filter === 'all' || this.filter === 'weapons'
         ? this.inventoryService.getPlayer()?.props.inventory.weapons?.length ??
-          0
+        0
         : 0);
     return Math.ceil(totalItems / this.itemsPerPage);
   }
 
   async updatePlayer(): Promise<void> {
     const player = this.inventoryService.getPlayer();
+    this.togglePopup()
+    setTimeout(() => {
+      this.togglePopup()
+    },1000)
     const response = await axios.post(
       'http://localhost:1803/player/update',
       { player }
     );
-
+    
     if (response && response.data) {
       console.log('actualizado', response.data);
+    }
+  }
+
+  togglePopup() {
+    let popup = document.getElementById("myPopup");
+    if (popup) {
+      popup.classList.toggle("show");
+      if(popup.textContent===''){
+        popup.textContent = 'Jugador actualizado';
+      }else{
+        popup.textContent = ''
+      }
     }
   }
 }
